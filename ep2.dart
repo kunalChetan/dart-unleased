4454fc7a282d1b4be7e1b00e83ea773fe09fbaf6
kunalpawar83 <kunalpawar8319@gmail.com>
2024-08-15 12:50:06 +0530

    ep2 - variables

---
+++ ep2.dart
@@ -0,0 +1,23 @@
+
+void main() {
+  String myString = 'Hello World!';
+  print(myString);
+  int myInt = 5;
+  print(myInt);
+
+  myString = 'Hello 2024';
+  print(myString);
+
+  double myDouble = 5.2;
+  print(myDouble);
+
+  myDouble = 5;
+  num myNum = 1;
+
+  myNum = 1.2;
+  print(myNum);
+
+  bool myBool = true;
+  print(myBool);
+
+}
