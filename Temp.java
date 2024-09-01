9f7874f09d474dbb07434eec56ae1f436d4fc4eb
kunalpawar83 <kunalpawar8319@gmail.com>
2024-09-01 14:39:40 +0530

    Temp

---
+++ Temp.java
@@ -0,0 +1,22 @@
+public class Temp {
+
+    public static void main(String [] args) {
+     String s = "Hello world";
+     s = null;
+     print(s.length());
+    }
+
+
+
+
+
+
+
+
+
+
+
+    static void print(int s) {
+        System.out.println(s);
+    }
+}
